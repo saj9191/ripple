9ce710924487ea778cd3d1d23c5a45216fe693a0
Shannon Joyner <shannonjoyner@Shannons-MacBook-Pro.local>
2018-06-16 17:34:55 -0400

    main: Switch back to comet executable
    Tide currently doesn't work on lambda. Until it's ready, we'll use
    comet and split up the process.

--- lambda/main.py
+++ lambda/main.py
@@ -6,44 +6,36 @@ import time
 s3 = boto3.resource('s3')
 
 def handler(event, context):
-  print("here")
-  bucket = s3.Bucket("human-fasta")
+#  bucket = s3.Bucket("human-fasta")
     
-  with open("/tmp/HUMAN.fasta.20170123", "wb") as f:
-    bucket.download_fileobj("HUMAN.fasta.20170123", f)
+#  with open("/tmp/HUMAN.fasta.20170123", "wb") as f:
+#    bucket.download_fileobj("HUMAN.fasta.20170123", f)
 
-  with open("/tmp/crux", "wb") as f:
-    bucket.download_fileobj("crux", f)
+#  with open("/tmp/crux", "wb") as f:
+#    bucket.download_fileobj("crux", f)
 
-  subprocess.call("chmod 755 /tmp/crux", shell=True)
-  index_files = ["auxlocs", "pepix", "protix"]
-  if not os.path.isdir("/tmp/HUMAN.fasta.20170123.index"):
-    os.mkdir("/tmp/HUMAN.fasta.20170123.index")
-  for index_file in index_files:
-    with open("/tmp/HUMAN.fasta.20170123.index/{0:s}".format(index_file), "wb") as f:
-      bucket.download_fileobj(index_file, f)
+#  subprocess.call("chmod 755 /tmp/crux", shell=True)
+#  index_files = ["auxlocs", "pepix", "protix"]
+#  if not os.path.isdir("/tmp/HUMAN.fasta.20170123.index"):
+#    os.mkdir("/tmp/HUMAN.fasta.20170123.index")
+#  for index_file in index_files:
+#    with open("/tmp/HUMAN.fasta.20170123.index/{0:s}".format(index_file), "wb") as f:
+#      bucket.download_fileobj(index_file, f)
      
-  print("yolo") 
-  output = subprocess.check_output("/tmp/crux tide-search /tmp/small.ms2 /tmp/HUMAN.fasta.20170123.index", shell=True)
-  print(output)
-  print("done")
-
-  print(subprocess.check_output(["ls", "/tmp"]))
-  return "Hello World!"
-
-
-#  output_file = "/tmp/small.txt"#.{0:s}-{1:s}.txt".format(event["start"], event["end"])
+  output_file = "/tmp/small.{0:s}-{1:s}.txt".format(event["start"], event["end"])
 #  command = "./crux tide-search small.ms2 HUMAN.fasta.20170123.index"
-#  command = "./comet.2018011.linux.exe -N/tmp/small sorted-small-{0:d}.ms2".format(event["start"])
-#-F{0:s} -L{1:s} sorted-small.ms2".format(event["start"], event["end"]) 
+  command = "./comet.2018011.linux.exe -N/tmp/small -F{0:s} -L{1:s} small.ms2".format(event["start"], event["end"])
 #  print(command)
-#  subprocess.call(command, shell=True)
+  subprocess.call(command, shell=True)
 
-#  done = False
-#  while not done:
-#    output = str(subprocess.check_output("ps aux | grep comet | grep small", shell=True))
-#    done = len(output.split("\n")) == 1
-#    time.sleep(1)
-#
-#  return os.path.isfile(output_file)
-#  return True
+  done = False
+  while not done:
+    process_output = str(subprocess.check_output("ps aux | grep comet | grep small", shell=True))
+    done = len(process_output.split("\n")) == 1
+    time.sleep(1)
+
+  if os.path.isfile(output_file):
+    output = open(output_file).read()
+  else:
+    output = ""
+  return output
