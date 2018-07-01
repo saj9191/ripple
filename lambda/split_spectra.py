97c61bdb06f24a0bb87affaa7c76f82f03ab7802
Shannon Joyner <shannonjoyner@dhcp-rhodes-2157.eduroam.cornell.edu>
2018-07-01 16:33:16 -0400

    split_spectra: Handle large files
    We only have 512MB of disk space on lambda. Spectra files can be a
    couple of gigabytes. As a result, we cannot store the entire file.
    To handle this, we change split spectra to read in the file in parts
    and create the spectra chunk files as we read in the file.

--- lambda/split_spectra.py
+++ lambda/split_spectra.py
@@ -5,48 +5,53 @@ import subprocess
 import time
 import util
 
-SPECTRA = re.compile("S\s\d+.*")
+SPECTRA = re.compile("S\s")
 
-def save_spectra(output_bucket, spectra, ts, num_files, i):
-  key = "spectra-{0:f}-{1:d}-{2:d}.ms2".format(ts, num_files, i)
-  output_bucket.put_object(Key=key, Body=str.encode("\n".join(spectra)))
+def save_spectra(output_bucket, header, spectra, ts, end_bytes, num_bytes, num_files, final):
+  key = "spectra-{0:f}-{1:d}-{2:d}-{3-d}.ms2".format(ts, num_files, end_bytes, num_bytes, suffix)
+  output_bucket.put_object(Key=key, Body=str.encode("{0:s}\n{1:s}".format(header, spectra)))
 
-def split_spectra(bucket_name, key, batch_size):
+def split_spectra(bucket_name, key, batch_size, chunk_size):
   util.clear_tmp()
   s3 = boto3.resource("s3")
   output_bucket = s3.Bucket("maccoss-human-split-spectra")
 
-  spectra = s3.Object(bucket_name, key).get()["Body"].read().decode("utf-8")
-
-  spectrum = []
-  spectra_subset = []
+  obj = s3.Object(bucket_name, key)
+  num_bytes = obj.content_length
   ts = time.time()
+  spectra = []
+  remainder = ""
+  header = None
 
-  i = 0
-  lines = spectra.split("\n")
-  num_spectra = list(filter(lambda line: "MS1Intensity" in line, lines))
-  num_files = int((len(num_spectra) + batch_size - 1) / batch_size)
+  start_byte = 0
+  num_files = 0
 
-  print("There are", len(lines), "of files spectra")
-  for line in lines:
-    if SPECTRA.match(line):
-      if len(spectrum) > 0:
-        spectra_subset.append("\n".join(spectrum))
+  while start_byte < num_bytes:
+    end_byte = min(start_byte + chunk_size, num_bytes)
+    stream = obj.get(Range="bytes={0:d}-{1:d}".format(start_byte, end_byte))["Body"].read().decode("utf-8")
+    stream = remainder + stream
+    if start_byte == 0:
+      parts = stream.split("\n")
+      header = parts[0]
+      stream = "\n".join(parts[1:])
+    parts = SPECTRA.split(stream)
+    parts = list(filter(lambda p: len(p) > 0, parts))
+    if len(parts) > 1:
+      spectra += parts[:-1]
+      while len(spectra) >= batch_size:
+        save_spectra(output_bucket, header, "S ".join(spectra[:batch_size]), ts, num_files, False)
+        spectra = spectra[batch_size:]
+        num_files += 1
 
-      spectrum = []
-      if len(spectra_subset) > batch_size:
-        save_spectra(output_bucket, spectra_subset, ts, num_files, i)
-        spectra_subset = []
-        i += 1
+    remainder = parts[-1]
+    start_byte = end_byte + 1
 
-    spectrum.append(line)
-
-  spectra_subset.append("\n".join(spectrum))
-  save_spectra(output_bucket, spectra_subset, ts, num_files, i)
+  print("number of files", num_files)
+  parts = spectra + [remainder]
+  save_spectra(output_bucket, header, "S ".join(spectra), ts, num_files, True)
 
 def handler(event, context):
   bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
   key = event["Records"][0]["s3"]["object"]["key"]
   params = json.loads(open("split_spectra.json").read())
-  split_spectra(bucket_name, key, params["batch_size"])
-
+  split_spectra(bucket_name, key, params["batch_size"], params["chunk_size"])
