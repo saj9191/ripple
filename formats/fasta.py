77d22ba76b4f7000e379bd84d69b9bafc88a14ea
Shannon Joyner <sj677@cornell.edu>
2018-07-27 16:02:38 -0400

    format: New FASTA format
    Also fix an offset bug for TSV.

--- formats/fasta.py
+++ formats/fasta.py
@@ -1,35 +1,14 @@
-import boto3
 import iterator
 
 
-class Combine():
-  def combine(bucket_name, keys, temp_name):
-    s3 = boto3.resource("s3")
-    f = open(temp_name, "w+")
-
-    for i in range(len(keys)):
-      key = keys[i]
-      content = s3.Object(bucket_name, key).get()["Body"].read().decode("utf-8")
-      if i == 0:
-        f.write(content)
-      else:
-        results = content.split("\n")[1:]
-        f.write("\n".join(results))
-
-
 class Iterator(iterator.Iterator):
+  ENTRY_IDENTIFIER = ">"
+
   def __init__(self, obj, batch_size, chunk_size):
     iterator.Iterator.__init__(self, obj, batch_size, chunk_size)
     self.total_count = None
     self.offsets = []
-
-    start_byte = 0
-    end_byte = start_byte + self.chunk_size
-    stream = self.getBytes(start_byte, end_byte)
-    self.current_offset = stream.indexOf("\n") + 1
-    # First element
-    self.offsets.append(self.current_offset)
-    self.getCount()
+    self.current_offset = 0
 
   def getCount(self):
     if self.total_count is not None:
@@ -40,14 +19,8 @@ class Iterator(iterator.Iterator):
     while start_byte < self.content_length:
       end_byte = start_byte + self.chunk_size
       stream = self.getBytes(start_byte, end_byte)
-      count += stream.count("\n")
+      count += stream.count(self.ENTRY_IDENTIFIER)
       start_byte = end_byte + 1
-
-    # Need to account for header line
-    if stream.endswith("\n"):
-      self.total_count = count
-    else:
-      self.total_count = count - 1
 
   def updateOffsets(self):
     start_byte = self.current_offset
@@ -55,10 +28,10 @@ class Iterator(iterator.Iterator):
     stream = self.getBytes(start_byte, end_byte)
     done = False
     while not done:
-      index = stream.index("\n")
+      index = stream.index(self.ENTRY_IDENTIFIER)
       done = (index == -1)
       if index != -1:
-        self.offsets.append(start_byte + index + 1)
+        self.offsets.append(start_byte + index)
         stream = stream[index + 1:]
     self.current_offset = end_byte + 1
 
