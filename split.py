33135a94ada5051f81e7943bf9a245fa8d46c420
Shannon Joyner <sj677@cornell.edu>
2018-07-31 01:06:02 -0400

    split: Turn into logic to split a file on disk

--- split.py
+++ split.py
@@ -1,50 +1,31 @@
 import boto3
-import json
-import spectra
-import util
+
+num_entries_per_file = 1*1000*1000
+num_entries = 370261379
+num_files = (num_entries + num_entries_per_file - 1) / num_entries_per_file
+
+s3 = boto3.resource("s3")
+bucket_name = "maccoss-smith-waterman-fasta"
 
 
-def split_spectra(key, bucket_name, batch_size, chunk_size):
-  s3 = boto3.resource("s3")
-  obj = s3.Object(bucket_name, key)
+def add(file_id, content):
+  key = "uniprot-fasta-{0:d}".format(file_id)
+  print("Adding key", key)
+  s3.Object(bucket_name, key).put(Body=str.encode(content))
 
-  m = util.parse_file_name(key)
-  ext = m["ext"]
 
-  if ext == "mzML":
-    iterator = spectra.mzMLSpectraIterator(obj, batch_size, chunk_size)
-  elif ext == "ms2":
-    iterator = spectra.ms2SpectraIterator(obj, batch_size, chunk_size)
+with open("data/uniprot-all.fasta", "r") as f:
+  count = 0
+  file_id = 1
+  content = ""
+  for line in f:
+    content += line
+    count += 1
+    if count == num_entries_per_file:
+      add(file_id, content)
+      file_id += 1
+      content = ""
+      count = 0
 
-  client = boto3.client("lambda")
-  more = True
-  file_id = 0
-  while more:
-    file_id += 1
-    [start_byte, end_byte, more] = iterator.nextOffsets()
-    payload = {
-      "Records": [{
-        "s3": {
-          "bucket": {
-            "name": bucket_name
-          },
-          "object": {
-            "key": key
-          },
-          "range": {
-            "file_id": file_id,
-            "start_byte": start_byte,
-            "end_byte": end_byte,
-            "more": more
-          }
-        }
-      }]
-    }
-    response = client.invoke(
-      FunctionName="AnalyzeSpectra",
-      InvocationType="Event",
-      Payload=json.JSONEncoder().encode(payload)
-    )
-    assert(response["ResponseMetadata"]["HTTPStatusCode"] == 202)
-
-  return file_id
+if len(content) > 0:
+  add(file_id, content)
