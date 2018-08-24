b0ed94da49c51c5cbb159a3f7ad7e7bcf70417c7
Shannon Joyner <sj677@cornell.edu>
2018-08-24 13:42:47 -0400

    match: Write match scores to log file
    Currently it's useful for the species identification step to know
    what each species scored, so we're writing the results to the log
    files.

--- lambda/match.py
+++ lambda/match.py
@@ -7,24 +7,28 @@ def find_match(bucket_name, key, input_format, output_format, offsets, params):
   util.print_read(input_format, key, params)
   [combine, keys] = util.combine_instance(bucket_name, key)
   if combine:
+    print("Finding match")
     best_match = None
     match_score = 0
     format_lib = importlib.import_module(params["format"])
     iterator = getattr(format_lib, "Iterator")
     s3 = boto3.resource("s3")
 
-    for key in keys:
-      obj = s3.Object(bucket_name, key)
-      it = iterator(obj, params["batch_size"], params["chunk_size"])
-      if params["find"] == "max sum":
-        score = it.sum(params["identifier"])
-      else:
-        raise Exception("Not implemented", params["find"])
+    keys.sort()
+    with open(util.LOG_NAME, "a+") as f:
+      for key in keys:
+        obj = s3.Object(bucket_name, key)
+        it = iterator(obj, params["batch_size"], params["chunk_size"])
+        if params["find"] == "max sum":
+          score = it.sum(params["identifier"])
+        else:
+          raise Exception("Not implemented", params["find"])
 
-      print("score", score, "key", key)
-      if score > match_score:
-        best_match = key
-        match_score = score
+        print("key {0:s} score {1:d}".format(key, score))
+        f.write("key {0:s} score {1:d}".format(key, score))
+        if score > match_score:
+          best_match = key
+          match_score = score
 
     output_format["ext"] = "match"
     output_format["suffix"] = "match"
