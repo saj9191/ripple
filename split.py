37250f32887f331cd1f56ba64b9ff570e2fe8a0b
Shannon Joyner <sj677@cornell.edu>
2018-09-16 08:58:29 -0400

    fix spilt logic

--- split.py
+++ split.py
@@ -1,18 +1,19 @@
 import boto3
 import util
 
-num_entries_per_file = 4*1000*1000
+num_entries_per_file = 90*1000
 num_entries = 370261379
 num_files = (num_entries + num_entries_per_file - 1) / num_entries_per_file
 
 [access_key, secret_key] = util.get_credentials("maccoss")
+print(access_key)
 session = boto3.Session(
   aws_access_key_id=access_key,
   aws_secret_access_key=secret_key,
   region_name="us-west-2"
 )
 s3 = session.resource("s3")
-bucket_name = "ssw-database"#"maccoss-smith-waterman-fasta"
+bucket_name = "ssw-database"
 #bucket = s3.Bucket(bucket_name)
 #bucket.objects.all().delete()
 
@@ -22,18 +23,22 @@ def add(file_id, content):
   s3.Object(bucket_name, key).put(Body=str.encode(content))
 
 
+
 with open("data/uniprot-all.fasta", "r") as f:
   count = 0
   file_id = 1
   content = ""
   for line in f:
+    if line.startswith(">"):
+      if count == num_entries_per_file:
+        print("Adding")
+        add(file_id, content)
+        file_id += 1
+        content = ""
+        count = 0
+      count += 1
     content += line
-    count += 1
-    if count == num_entries_per_file:
-      add(file_id, content)
-      file_id += 1
-      content = ""
-      count = 0
+
 
 if len(content) > 0:
   add(file_id, content)
