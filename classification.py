ffc7275eb70e8a117f356587dadad58907455bcf
Shannon Joyner <sj677@cornell.edu>
2018-09-20 11:53:00 -0400

    Rework classification

--- classification.py
+++ classification.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import random
 import re
+import time
 import util
 
 SOLUTION_REGEX = re.compile("([A-Za-z0-9_]+),([0-9]+),\"POLYGON \(([0-9\-\.\, \(\)]+)\).*")
@@ -45,7 +46,7 @@ def on_border(polygon, x, y):
     [ix, iy] = polygon[i]
     [jx, jy] = polygon[j]
     cross_prod = (y - iy) * (jx - ix) - (x - ix) * (jy - iy)
-    if abs(cross_prod) > 0.1:
+    if abs(cross_prod) > 10:
       continue
 
     dot_prod = (x - ix) * (jx - ix) + (y - iy) * (jy - iy)
@@ -86,15 +87,12 @@ def in_polygon(polygon, x, y):
   return inside
 
 
-def write_classification(im, f, pixels, clz):
-  random.shuffle(pixels)
-  pixels = pixels[:300]
-  for [x, y] in pixels:
-    [r, g, b] = im[y, x]
-    f.write("{x} {y} {r} {g} {b} {c}\n".format(x=x, y=y, r=r, g=g, b=b, c=clz))
+def write_classification(f, pixels):
+  for [r, g, b, clz] in pixels:
+    f.write("{r} {g} {b} {c}\n".format(r=r, g=g, b=b, c=clz))
 
 
-def create_classifications(s3, folder, image_name, polygons):
+def create_classifications(s3, folder, image_name, polygons, border, inside, outside):
   im = plt.imread(folder + "/" + image_name)
   [height, width, dim] = im.shape
   border = []
@@ -103,40 +101,63 @@ def create_classifications(s3, folder, image_name, polygons):
 
   for y in range(height):
     for x in range(width):
+      [r, g, b] = im[y, x]
       if len(polygons) == 0:
         outside.append([x, y])
       else:
         found = False
         for polygon in polygons:
           if on_border(polygon, x, y):
-            border.append([x, y])
+            border.append([r, g, b, BORDER])
             found = True
             break
           elif in_polygon(polygon, x, y):
-            inside.append([x, y])
+            inside.append([r, g, b, INSIDE])
             found = True
         if not found:
-          outside.append([x, y])
-
-  key = image_name.replace("tif", "classification")
-  temp_name = "/tmp/{0:s}".format(key)
-  if len(border) > 0 or len(inside) > 0:
-    print("border", len(border), "inside", len(inside), "outside", len(outside))
-  with open(temp_name, "w+") as f:
-    write_classification(im, f, border, BORDER)
-    write_classification(im, f, inside, INSIDE)
-    write_classification(im, f, outside, OUTSIDE)
-
-  s3.Object("maccoss-spacenet", key).put(Body=open(temp_name, "rb"))
+          outside.append([r, g, b, OUTSIDE])
 
 
 def process_images(folder, solutions, params):
   s3 = util.s3(params)
-  image_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))][:100]
+  image_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+  random.shuffle(image_names)
+  image_names = image_names[:200]
+  border = set()
+  inside = set()
+  outside = set()
+  total_files = 100
+  num_items_per_file = 100 * 1000
+  third = num_items_per_file / 3
+  file_id = 1
+
+  i = 0
   for image_name in image_names:
+    print(time.time(), i, image_name)
     name = image_name.replace("3band_", "").replace(".tif", "")
     solution = solutions[name]
-    create_classifications(s3, folder, image_name, solution)
+    create_classifications(s3, folder, image_name, solution, border, inside, outside)
+    if len(border) > third and len(inside) > third and len(outside) > third:
+      break
+    i += 1
+
+
+  classifications = border.union(inside).union(outside)
+  random.seed(0)
+  random.shuffle(classifications)
+
+  index = 0
+  while file_id <= total_files and index < len(classifications):
+    print("Creating file", file_id)
+    key = "classification-{0:d}.classification"
+    temp_name = "/tmp/{0:s}".format(key)
+    with open(temp_name, "w+") as f:
+      items = classifications[index:index + num_items_per_file]
+      write_classification(f, items)
+      index += len(items)
+    s3.Object("maccoss-spacenet", key).put(Body=open(temp_name, "rb"))
+    file_id += 1
+
 
 
 solutions = parse_solutions("competition1/spacenet_TrainData/vectordata/summarydata/AOI_1_RIO_polygons_solution_3band.csv")
