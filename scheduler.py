c254faeaaf1cafe8303af3ab865af3ae543f9366
Shannon Joyner <sj677@cornell.edu>
2018-09-23 00:51:10 -0400

    Multi-thread version

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,67 @@
 import argparse
 import json
+import queue
+import threading
 import time
 import util
 
 
-def schedule(params):
-  # TODO: For now, just make a list
-  pipeline = params["pipeline"]
-  functions = params["functions"]
-  start_key = "0/1537670657.440274-565/1/1-1-tide.mzML"
-  running = []
-  pending = [
+class Worker(threading.Thread):
+  def __init__(self, worker_id, pending, timeout, params, event, client):
+    super(Worker, self).__init__()
+    self.worker_id = worker_id
+    self.pending = pending
+    self.running = True
+    self.timeout = timeout
+    self.params = dict(params)
+    self.event = event
+    self.client = client
+
+  def run(self):
+    functions = self.params["functions"]
+    pipeline = self.params["pipeline"]
+    s3 = util.s3(self.params)
+
+    while self.running:
+      try:
+        [prefix, payload] = self.pending.get(timeout=self.timeout)
+        if prefix < len(pipeline):
+          name = pipeline[prefix]["name"]
+          util.invoke(self.client, name, self.params, payload)
+          s = payload["Records"][0]["s3"]
+          input_key = s["object"]["key"]
+          rparams = {**self.params, **s}
+          rparams["prefix"] = prefix
+          rparams["scheduler"] = True
+          [input_format, output_format, bucket_format] = util.get_formats(input_key, functions[name]["file"], rparams)
+          log_file = util.file_name(bucket_format)
+          while not util.object_exists(self.params["log"], log_file):
+            time.sleep(5)
+          content = s3.Object(self.params["log"], log_file).get()["Body"].read()
+          lparams = json.loads(content.decode("utf-8"))
+          for p in lparams["payloads"]:
+            self.pending.put([prefix + 1, p])
+        else:
+          print("Worker", self.worker_id, "Found final")
+          self.event.set()
+      except queue.Empty:
+        time.sleep(5)
+
+
+def schedule(start_key, params):
+  max_running_lambdas = 1000
+  pending = queue.Queue()
+  workers = []
+  params["object"] = {}
+  params["scheduler"] = False
+  timeout = 10
+  event = threading.Event()
+  client = util.setup_client("lambda", params)
+  for i in range(max_running_lambdas):
+    workers.append(Worker(i, pending, timeout, params, event, client))
+    workers[-1].start()
+
+  pending.put(
     [
       0,
       {
@@ -26,51 +77,13 @@ def schedule(params):
         }]
       }
     ]
-  ]
-  params["object"] = {}
-  max_running_lambdas = 1000
-  client = util.setup_client("lambda", params)
-  s3 = util.s3(params)
-  stages = set()
+  )
+  event.wait()
+  for worker in workers:
+    worker.running = False
 
-  while (len(running) + len(pending)) > 0:
-    i = 0
-    print("Running", len(running), "Pending", len(pending))
-    while i < len(running):
-      log_file = running[i]
-      m = util.parse_file_name(log_file)
-      prefix = m["prefix"] - 1
-      name = pipeline[prefix]["name"]
-      params["prefix"] = prefix
-      # TODO: Need to know what stage we're on
-      params["scheduler"] = True
-      if util.object_exists(params["log"], log_file):
-        content = s3.Object(params["log"], log_file).get()["Body"].read()
-        lparams = json.loads(content.decode("utf-8"))
-        pending += list(map(lambda p: [prefix + 1, p], lparams["payloads"]))
-        del running[i]
-      else:
-        i += 1
-
-    while len(running) < max_running_lambdas and len(pending) > 0:
-      [prefix, payload] = pending.pop(0)
-      if prefix < len(pipeline):
-        params["scheduler"] = False
-        name = pipeline[prefix]["name"]
-        util.invoke(client, name, params, payload)
-        s = payload["Records"][0]["s3"]
-        input_key = s["object"]["key"]
-        rparams = {**params, **s}
-        rparams["prefix"] = prefix
-        rparams["scheduler"] = True
-        [input_format, output_format, bucket_format] = util.get_formats(input_key, functions[name]["file"], rparams)
-        log_file = util.file_name(bucket_format)
-        running.append(log_file)
-        if prefix not in stages:
-          print("Starting stage", prefix, name)
-          stages.add(prefix)
-
-    time.sleep(5)
+  for worker in workers:
+    worker.join()
 
 
 def main():
