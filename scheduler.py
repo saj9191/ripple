667ffd60730bdd7c8a79a71ce77a548da885ac46
Shannon Joyner <sj677@cornell.edu>
2018-09-25 10:52:34 -0400

    Slowly transition scheduler to fault tolerance

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 import argparse
+import botocore
 import json
 import queue
 import threading
@@ -7,14 +8,18 @@ import util
 
 
 class Item:
-  def __init__(self, priority, prefix, job_id, payload):
+  def __init__(self, scheduler, priority, prefix, job_id, payload):
     self.priority = priority
     self.prefix = prefix
     self.payload = payload
     self.job_id = job_id
+    self.scheduler = scheduler
 
   def __lt__(self, other):
-    return self.priority < other.priority
+    if self.scheduler == "robin":
+      return self.priority < other.priority
+    elif self.scheduler == "fifo":
+      return [self.job_id, self.priority] < [other.job_id, other.priority]
 
 
 class PriorityQueue:
@@ -41,6 +46,7 @@ class PriorityQueue:
   def get(self):
     if self.scheduler == "fifo":
       item = self.queue.get(timeout=0)
+      return item
     elif self.scheduler == "robin":
       count = 0
       while count < len(self.queue):
@@ -57,7 +63,7 @@ class PriorityQueue:
 
 
 class Worker(threading.Thread):
-  def __init__(self, worker_id, scheduler, pending, q, params, condition, client, results):
+  def __init__(self, worker_id, scheduler, pending, q, params, condition, results):
     super(Worker, self).__init__()
     self.worker_id = worker_id
     self.pending = pending
@@ -65,7 +71,6 @@ class Worker(threading.Thread):
     self.running = True
     self.params = dict(params)
     self.condition = condition
-    self.client = client
     self.results = results
     self.index = 0
     self.scheduler = scheduler
@@ -91,12 +96,11 @@ class Worker(threading.Thread):
     pipeline = self.params["pipeline"]
     while self.running:
       try:
-        item = self.pending.get()
+        item = self.pending.get(timeout=5)
         prefix = item.prefix
+        name = pipeline[prefix]["name"]
         payload = item.payload
         if prefix < len(pipeline):
-          name = pipeline[prefix]["name"]
-          util.invoke(self.client, name, self.params, payload)
           s = payload["Records"][0]["s3"]
           input_key = s["object"]["key"]
           rparams = {**self.params, **s}
@@ -104,19 +108,18 @@ class Worker(threading.Thread):
           rparams["scheduler"] = True
           [input_format, output_format, bucket_format] = util.get_formats(input_key, functions[name]["file"], rparams)
           log_file = util.file_name(bucket_format)
-          self.queue.put([item.job_id, log_file])
+          self.queue.put([item.job_id, log_file, item, time.time()])
         else:
-          print("Worker", self.worker_id, "Found final")
           self.results.put(item)
           self.condition.acquire()
           self.condition.notify()
           self.condition.release()
       except queue.Empty:
-        time.sleep(5)
+        pass
 
 
 class Parser(threading.Thread):
-  def __init__(self, parser_id, scheduler, q, pending, params):
+  def __init__(self, parser_id, scheduler, q, pending, params, client):
     super(Parser, self).__init__()
     self.handler_id = parser_id
     self.queue = q
@@ -126,21 +129,34 @@ class Parser(threading.Thread):
     self.running = True
     self.log_length = 1000
     self.scheduler = scheduler
+    self.client = client
 
   def run(self):
     s3 = util.s3(self.params)
+    pipeline = self.params["pipeline"]
     while self.running:
       i = 0
       while i < len(self.logs):
-        [job_id, log_file] = self.logs[i]
-        if util.object_exists(self.params["log"], log_file):
+        [job_id, log_file, item, start_time] = self.logs[i]
+        objs = list(map(lambda o: o.key, util.get_objects(self.params["bucket"], log_file[:log_file.rindex("-")], self.params))).sort()
+        if len(objs) > 0:
           self.logs = self.logs[:i] + self.logs[i + 1:]
-          prefix = util.parse_file_name(log_file)["prefix"]
-          obj = s3.Object(self.params["log"], log_file)
-          content = obj.get()["Body"].read()
-          lparams = json.loads(content.decode("utf-8"))
-          for p in lparams["payloads"]:
-            self.pending.put(Item(obj.last_modified, prefix, job_id, p))
+          prefix = util.parse_file_name(objs[0])["prefix"]
+          done = False
+          while not done:
+            try:
+              obj = s3.Object(self.params["log"], log_file)
+              content = obj.get()["Body"].read()
+              lparams = json.loads(content.decode("utf-8"))
+              for p in lparams["payloads"]:
+                self.pending.put(Item(self.scheduler, obj.last_modified, prefix, job_id, p))
+              done = True
+            except botocore.errorfactory.NoSuchKey:
+              time.sleep(1)
+        elif (time.time() - start_time) > 5*60:
+          prefix = item.prefix
+          name = pipeline[prefix]["name"]
+          util.invoke(self.client, name, self.params, payload)
         else:
           i += 1
 
@@ -150,59 +166,68 @@ class Parser(threading.Thread):
         except queue.Empty:
           pass
 
-      time.sleep(1)
+
+def payload(bucket, key):
+  return {
+   "Records": [{
+     "s3": {
+        "bucket": {
+         "name": bucket,
+        },
+        "object": {
+          "key": key,
+        },
+      }
+    }]
+  }
 
 
-def schedule(start_keys, params):
-  max_workers = 100
-  max_parsers = 100
-  results = queue.Queue()
-  q = queue.Queue()
-  workers = []
-  params["object"] = {}
-  params["scheduler"] = False
-  condition = threading.Condition()
-  client = util.setup_client("lambda", params)
-  scheduler = "robin"
+class Scheduler:
+  def __init__(self, scheduler, params):
+    self.params = dict(params)
+    self.params["object"] = {}
+    self.params["scheduler"] = False
+    self.scheduler = scheduler
+    self.q = queue.Queue()
+    self.condition = threading.Condition()
+    self.results = queue.Queue()
+    self.client = util.setup_client("lambda", self.params)
+    self.pending = PriorityQueue(self.scheduler)
+    self.workers = []
 
-  pending = PriorityQueue(scheduler)
-  for i in range(max_workers):
-    workers.append(Worker(i, scheduler, pending, q, params, condition, client, results))
-    workers[-1].start()
+  def setup(self):
+    max_workers = 100
+    max_parsers = 100
 
-  for i in range(max_parsers):
-    workers.append(Parser(i, scheduler, q, pending, params))
-    workers[-1].start()
+    for i in range(max_workers):
+      self.workers.append(Worker(i, self.scheduler, self.pending, self.q, self.params, self.condition, self.results))
+      self.workers[-1].start()
 
-  job_id = 0
-  for job_id in range(len(start_keys)):
-    start_key = start_keys[job_id]
-    m = util.parse_file_name(start_key)
-    item = Item(m["timestamp"], 0, job_id, {
-      "Records": [{
-        "s3": {
-          "bucket": {
-            "name": params["bucket"],
-          },
-          "object": {
-            "key": start_key,
-          },
-        }
-      }]
-    })
-    pending.put(item)
+    for i in range(max_parsers):
+      self.workers.append(Parser(i, self.scheduler, self.q, self.pending, self.params, self.client))
+      self.workers[-1].start()
 
-  while results.qsize() < params["num_output"] * len(start_keys):
-    print("Actual", results.qsize(), "Expected", params["num_output"])
-    condition.acquire()
-    condition.wait()
-    condition.release()
+    # job_id = 0
+    # for job_id in range(len(start_keys)):
+    # start_key = start_keys[job_id]
+    # m = util.parse_file_name(start_key)
+    # item = Item(m["timestamp"], 0, job_id, payload(params["bucket"], start_key))
+    # pending.put(item)
+    return self.pending
 
-  for worker in workers:
-    worker.running = False
+  def wait(self, concurrency):
+    while self.results.qsize() < self.params["num_output"] * concurrency:
+      print("Actual", self.results.qsize(), "Expected", self.params["num_output"] * concurrency)
+      self.condition.acquire()
+      self.condition.wait()
+      self.condition.release()
 
-  for worker in workers:
-    worker.join()
+    print("Shutting down")
+    for worker in self.workers:
+      worker.running = False
+
+    for worker in self.workers:
+      worker.join()
 
 
 def main():
@@ -214,11 +239,7 @@ def main():
   [access_key, secret_key] = util.get_credentials(params["credential_profile"])
   params["access_key"] = access_key
   params["secret_key"] = secret_key
-  start_keys = [
-    "0/1537682868.154564-338/1/1-1-tide.tif",
-    "0/1537712854.948869-887/1/1-1-tide.tif"
-  ]
-  schedule(start_keys, params)
+#  schedule(start_keys, params)
 
 
 if __name__ == "__main__":
