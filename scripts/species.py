4792e8dd19d1e11499ef9fba8d77a44595cecc6c
Shannon <sj677@cornell.edu>
2018-11-27 11:13:46 -0500

    species: Update to take command line arguments

--- scripts/species.py
+++ scripts/species.py
@@ -1,58 +1,19 @@
+import argparse
 import boto3
-import clear
+import inspect
 import json
+import os
 import print_species
+import sys
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import clear
 import setup
 import statistics
 import upload
 import util
 
-params = json.loads(open("json/private-tide.json").read())
-params["sample_bucket"] = "tide-source-data"
-params["folder"] = "tide"
-[access_key, secret_key] = util.get_credentials(params["credential_profile"])
-params["access_key"] = access_key
-params["secret_key"] = secret_key
-
-setup.setup(params)
-
-s3 = boto3.resource("s3")
-sample_bucket_name = "tide-source-data"
-sample_bucket = s3.Bucket(sample_bucket_name)
-objects = list(sample_bucket.objects.filter(Prefix="Differential_analysis_of_a_synaptic_density_fraction_from_Homer2_knockout_and_wild-type_olfactory_bulb_digests-1534804159922"))
-
-file_to_results = {}
-file_to_costs = {}
-
-for obj in objects:
-  key = obj.key
-  s3_key, _, _ = upload.upload(params["bucket"], key, sample_bucket_name)
-  token = s3_key.split("/")[1]
-  params["key"] = s3_key
-  species_to_score = print_species.run()
-  file_to_results[key] = species_to_score
-  [costs, _] = statistics.statistics(params["log"], token=token, prefix=None, params=params, show=False)
-  file_to_costs[key] = costs
-  clear.clear(params["bucket"], None, None)
-  clear.clear(params["log"], None, None)
-
-keys = list(file_to_results.keys())
-keys.sort()
-
-f = open("results.csv", "w+")
-# Header line
-for key in keys:
-  f.write(",{0:s}".format(key))
-f.write("\n")
-
-# Cost line
-f.write("Cost")
-for key in keys:
-  f.write(",{0:f}".format(file_to_costs[key][-1]))
-f.write("\n")
-
-fastas = list(file_to_results[keys[0]].keys())
-fastas.sort()
 
 species = [
   "Arabidopsis",
@@ -72,17 +33,89 @@ species = [
   "Zebrafish"
 ]
 
-sorted_fastas = []
-for specie in species:
-  for fasta in fastas:
-    if fasta.endswith(specie):
-      sorted_fastas.append(fasta)
 
-# Fasta scores
-for fasta in sorted_fastas:
-  f.write(fasta)
+def run(folder, top):
+  os.chdir("..")
+  params = json.loads(open("json/private-tide.json").read())
+  params["sample_bucket"] = "tide-source-data"
+  params["folder"] = "tide"
+  [access_key, secret_key] = util.get_credentials(params["credential_profile"])
+  params["access_key"] = access_key
+  params["secret_key"] = secret_key
+
+  setup.setup(params)
+
+  s3 = boto3.resource("s3")
+  sample_bucket_name = "tide-source-data"
+  sample_bucket = s3.Bucket(sample_bucket_name)
+  objects = list(sample_bucket.objects.filter(Prefix=folder))
+
+  data_folder = "scripts/data_counts"
+  if not os.path.isdir(data_folder):
+    os.mkdir(data_folder)
+
+  top_folder = data_folder + "/" + str(top)
+  if not os.path.isdir(top_folder):
+    os.mkdir(top_folder)
+
+  file_to_results = {}
+  file_to_costs = {}
+
+  for obj in objects:
+    key = obj.key
+    s3_key, _, _ = upload.upload(params["bucket"], key, sample_bucket_name)
+    token = s3_key.split("/")[1]
+    params["key"] = s3_key
+    species_to_score = print_species.run()
+    file_to_results[key] = species_to_score
+    [costs, _] = statistics.statistics(params["log"], token=token, prefix=None, params=params, show=False)
+    file_to_costs[key] = costs
+    clear.clear(params["bucket"], None, None)
+    clear.clear(params["log"], None, None)
+
+  keys = list(file_to_results.keys())
+  keys.sort()
+
+  bucket_file = top_folder + "/" + folder + ".csv"
+
+  f = open(bucket_file, "w+")
+  # Header line
   for key in keys:
-    f.write(",{0:d}".format(file_to_results[key][fasta]))
+    f.write(",{0:s}".format(key))
   f.write("\n")
 
-f.close()
+  # Cost line
+  f.write("Cost")
+  for key in keys:
+    f.write(",{0:f}".format(file_to_costs[key][-1]))
+  f.write("\n")
+
+  fastas = list(file_to_results[keys[0]].keys())
+  fastas.sort()
+
+  sorted_fastas = []
+  for specie in species:
+    for fasta in fastas:
+      if fasta.endswith(specie):
+        sorted_fastas.append(fasta)
+
+  # Fasta scores
+  for fasta in sorted_fastas:
+    f.write(fasta)
+    for key in keys:
+      f.write(",{0:d}".format(file_to_results[key][fasta]))
+    f.write("\n")
+
+  f.close()
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--folder", type=str, required=True, help="Name of folder file resides in")
+  parser.add_argument("--top", type=int, required=True, help="Number of top spectra to use")
+  args = parser.parse_args()
+  run(args.folder, args.top)
+
+
+if __name__ == "__main__":
+  main()
