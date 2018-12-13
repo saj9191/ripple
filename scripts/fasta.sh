61f709651a13e27082af046de069821afd485408
Shannon <sj677@cornell.edu>
2018-12-13 13:08:27 -0500

    Updates to fasta

--- scripts/fasta.sh
+++ scripts/fasta.sh
@@ -29,6 +29,10 @@ process()
   done
 }
 
+
+#process "superSilacLys4Arg6Lys8Arg10" "--mods-spec 1K+4.025107 --mods-spec 1R+6.020129 --mods-spec 1K+8.014199 --mods-spec 1R+10.008269"
+#process "silacLys8Arg10" "--mods-spec 1K+8.014199 --mods-spec 1R+10.008269"
+process "silacLys6Arg6" "--mods-spec 1KR+6.020129"
 #process "normal" "--missed-cleavages 1 --mods-spec 1M+15.9949"
 #process "itraq4" "--missed-cleavages 1  --mods-spec 1M+15.9949 --mods-spec K+144.10253 --nterm-peptide-mods-spec X+144.10253"
 #process "itraq8" "--missed-cleavages 1  --mods-spec 1M+15.9949 --mods-spec K+304.2022 --nterm-peptide-mods-spec X+304.2022"
@@ -39,19 +43,19 @@ process()
 #process "itraq2" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+225.155833 --nterm-peptide-mods-spec X+225.155833"
 #process "silac4" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec 2R+3.988140"
 #process "phosphorylation" "--mods-spec 1STY+79.966331"
-process "phosphorylationCleav" "--missed-cleavages 1 --mods-spec 1STY+79.966331"
-process "itraq4Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+144.10253 --nterm-peptide-mods-spec X+144.10253"
-process "itraq8Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+304.2022 --nterm-peptide-mods-spec X+304.2022"
-process "itraq2Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+225.155833 --nterm-peptide-mods-spec X+225.155833"
-process "tmt6Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+229.162932 --nterm-peptide-mods-spec X+229.162932"
-return
+#process "phosphorylationCleav" "--missed-cleavages 1 --mods-spec 1STY+79.966331"
+#process "itraq4Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+144.10253 --nterm-peptide-mods-spec X+144.10253"
+#process "itraq8Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+304.2022 --nterm-peptide-mods-spec X+304.2022"
+#process "itraq2Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+225.155833 --nterm-peptide-mods-spec X+225.155833"
+#process "tmt6Phosphorylation" "--missed-cleavages 1  --mods-spec 1STY+79.966331 --mods-spec K+229.162932 --nterm-peptide-mods-spec X+229.162932"
+#return
 
 
-process "normal" "--missed-cleavages 1 --mods-spec 1M+15.9949"
-process "silac8" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec 2K+8.014199"
-process "itraq8" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+304.2022 --nterm-peptide-mods-spec X+304.2022"
-process "itraq2" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+225.155833 --nterm-peptide-mods-spec X+225.155833"
-process "tmt6" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+229.162932 --nterm-peptide-mods-spec X+229.162932"
-process "silac4" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec 2R+3.988140"
+#process "normal" "--missed-cleavages 1 --mods-spec 1M+15.9949"
+#process "silac8" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec 2K+8.014199"
+#process "itraq8" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+304.2022 --nterm-peptide-mods-spec X+304.2022"
+#process "itraq2" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+225.155833 --nterm-peptide-mods-spec X+225.155833"
+#process "tmt6" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec K+229.162932 --nterm-peptide-mods-spec X+229.162932"
+#process "silac4" "--missed-cleavages 1 --mods-spec 1M+15.9949 --mods-spec 2R+3.988140"
 
 
