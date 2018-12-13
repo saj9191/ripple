7c7922cee89d3eda71302931b1f2f2b431b3786a
Shannon <sj677@cornell.edu>
2018-12-13 13:06:56 -0500

    Update to show duration

--- statistics.py
+++ statistics.py
@@ -2,13 +2,44 @@ import argparse
 import boto3
 import json
 import setup
+import sys
 import util
+
+
+def process_objects(s3, bucket_name, objects, params):
+  messages = []
+  costs = {-1: 0}
+  durations = {-1: [sys.maxsize, 0]}
+  memory_parameters = json.loads(open("json/memory.json").read())
+
+  for objSum in objects:
+    obj_format = util.parse_file_name(objSum.key)
+    obj = s3.Object(bucket_name, objSum.key)
+    body = json.loads(obj.get()["Body"].read().decode("utf-8"))
+    duration = body["duration"]
+
+    for prefix in [-1, obj_format["prefix"]]:
+      if prefix not in costs:
+        costs[prefix] = 0
+        durations[prefix] = [sys.maxsize, 0]
+      costs[prefix] += (body["write_count"] + body["list_count"]) / 1000.0 * 0.005
+      costs[prefix] += body["read_count"] / 1000.0 * 0.0004
+      memory_size = str(params["functions"][body["name"]]["memory_size"])
+      costs[prefix] += memory_parameters["lambda"][memory_size] * int((duration + 99) / 100)
+      start_time = body["start_time"]
+      end_time = start_time + body["duration"] / 1000.0
+
+      for p in [-1, prefix]:
+        durations[p][0] = min(durations[p][0], start_time)
+        durations[p][1] = max(durations[p][1], end_time)
+
+    messages.append({"key": objSum.key, "body": body})
+  return [messages, costs, durations]
 
 
 def statistics(bucket_name, token, prefix, params, show=False):
   s3 = boto3.resource("s3")
   bucket = s3.Bucket(bucket_name)
-  memory_parameters = json.loads(open("json/memory.json").read())
   if prefix is None and token is None:
     objects = list(bucket.objects.all())
   elif prefix is None and token is not None:
@@ -19,34 +50,20 @@ def statistics(bucket_name, token, prefix, params, show=False):
   else:
     objects = list(bucket.objects.filter(Prefix=str(prefix) + "/" + token))
 
-  objects.sort(key=lambda obj: obj.key)
-  messages = []
-  costs = {-1: 0}
-  for objSum in objects:
-    obj_format = util.parse_file_name(objSum.key)
-    obj = s3.Object(bucket_name, objSum.key)
-    body = json.loads(obj.get()["Body"].read().decode("utf-8"))
-    duration = body["duration"]
+  [messages, costs, durations] = process_objects(s3, bucket_name, objects, params)
 
-    for prefix in [-1, obj_format["prefix"]]:
-      if prefix not in costs:
-        costs[prefix] = 0
-      costs[prefix] += (body["write_count"] + body["list_count"]) / 1000.0 * 0.005
-      costs[prefix] += body["read_count"] / 1000.0 * 0.0004
-      memory_size = str(params["functions"][body["name"]]["memory_size"])
-      costs[prefix] += memory_parameters["lambda"][memory_size] * int((duration + 99) / 100)
-
-    if show:
-      print(obj)
-      print(body)
-      print("")
-    messages.append({"key": objSum.key, "body": body})
-
+  print("Section Costs")
   for prefix in costs.keys():
     if prefix != -1:
       print(prefix, costs[prefix])
 
   print("Total Cost", costs[-1])
+  print("Section Durations")
+  for prefix in durations.keys():
+    if prefix != -1:
+      print(prefix, durations[prefix][1] - durations[prefix][0], "seconds")
+
+  print("Total Duration", durations[-1][1] - durations[-1][0], "secionds")
   return [costs, messages]
 
 
@@ -59,7 +76,7 @@ def main():
   args = parser.parse_args()
   params = json.loads(open(args.parameters).read())
   setup.process_functions(params)
-  statistics(args.bucket_name, args.token, args.prefix, params, show=True)
+  statistics(args.bucket_name, args.token, args.prefix, params, show=False)
 
 
 if __name__ == "__main__":
