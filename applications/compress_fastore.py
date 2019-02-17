53c39057fd995c5141fe3d7b52dd3240b92d103b
jesse <jessewjx@163.com>
2019-02-16 20:19:00 -0500

    compress successfully version

--- applications/compress_fastore.py
+++ applications/compress_fastore.py
@@ -11,24 +11,39 @@ def run(database: Database, file: str, params, input_format, output_format, offs
         os.makedirs(dir_path)
 
     for entry in database.get_entries(params["program_bucket"]):
-      with open(dir_path + "/" + entry.key) as f:
+      with open(dir_path + entry.key,"wb+") as f:
         entry.download(f)
 
-    subprocess.call("chmod 755 /tmp/fastore_test",shell=True)
+
+    for (root, dirs, files) in os.walk(dir_path, topdown=True):
+        for ele in files:
+            ele = ''.join(ele)
+            print(ele)
+            subprocess.call("chmod 755 "+ dir_path +ele,shell = True)
+
 
     input_file = file
-    output_file = "/tmp/{0:s}".format(util.file_name(output_format))
+    tmp_file = "/tmp/{0:s}".format(util.file_name(output_format)).split("/")[:-1]
+    output_file = "/".join(tmp_file)
+    output_file = os.path.join(output_file, "1-1-1-output")
+    
+
     arguments = [
         "in " + input_file,
         "pair " + input_file,
-        "out " + os.path.join(output_file, "OUTPUT"),
-        # "threads 2"
+        "out " + output_file
     ]
-    output_file = "/tmp/{0:s}".format(util.file_name(output_format))
 
-    command = "cd /tmp; ./fastore_compress.sh --lossless --{0:s} > {2:s}".format(" --".join(arguments),output_file)
+    command = "cd /tmp/fastore_test; ./fastore_compress.sh --lossless --{0:s}".format(" --".join(arguments),output_file)
     subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    return[output_file]
+
+    output_list = []
+    for (root,dirs,files) in os.walk(output_file,topdown = True):
+        for file in files:
+            if file.endswith(".cmeta") or file.endswith(".cdata"):
+                output_list.append(os.path.join(output_file, file))
+
+    return output_list
 
 
 
