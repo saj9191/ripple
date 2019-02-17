1cbe97b394f5abf1325e4cee108836168d14fb7c
jesse <jessewjx@163.com>
2019-02-16 20:23:28 -0500

    pipeline_fastore_test

--- tests/pipeline_fastore_test.py
+++ tests/pipeline_fastore_test.py
@@ -2,15 +2,19 @@ import pipeline
 import unittest
 from tutils import TestEntry, TestTable
 from typing import Any, Dict
+import os
 
 
-class SmithWaterman(unittest.TestCase):
+class FaStore(unittest.TestCase):
   def test_basic(self):
+    directory = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(directory)
+    
     pp: pipeline.Pipeline = pipeline.Pipeline("fastore/fastore_compression.json")
 
-    files = ["fastore_bin", "fastore_rebin", "fastore_pack"]
+    files = ["fastore_bin", "fastore_rebin", "fastore_pack","fastore_compress.sh"]
 
-    pp.populate_table("jessie-fastore-program", "fastore/", [])
+    pp.populate_table("jessie-fastore-program", "fastore/", files)
 
     name = "0/123.400000-13/1-1/1-1-1-fasta.fq"
     pp.run(name, "fastore/SP1.fq")
