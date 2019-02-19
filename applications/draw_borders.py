3442d50aa239d19e93e4e71183e99ce92492e919
Shannon <sj677@cornell.edu>
2019-02-18 23:27:50 -0500

    draw_borders: Update to use flooding

--- applications/draw_borders.py
+++ applications/draw_borders.py
@@ -13,12 +13,42 @@ class Indices(Enum):
   outside = 2
 
 
+def flood(classifications, x, y, visited, width, height, cl):
+  stack = [(x, y)]
+
+  while len(stack) > 0:
+    [x, y] = stack.pop(0)
+    if x >= width or y >= height:
+      continue
+    if x < 0 or y < 0:
+      continue
+    if x in visited and y in visited[x]:
+      continue
+    if x not in visited:
+      visited[x] = set()
+    visited[x].add(y)
+
+    # Check if border, if so stop
+    if classifications[y][x] == Indices.border.value:
+      continue
+
+    classifications[y][x] = cl
+    stack.append((x - 1, y - 1))
+    stack.append((x - 1, y))
+    stack.append((x - 1, y + 1))
+    stack.append((x, y - 1))
+    stack.append((x, y + 1))
+    stack.append((x + 1, y - 1))
+    stack.append((x + 1, y))
+    stack.append((x + 1, y + 1))
+
+
 def run(database: Database, key: str, params, input_format, output_format, offsets: List[int]):
   entries = database.get_entries(params["bucket"], str(params["input_prefix"]) + "/")
   assert(len(entries) == 1)
   entry = entries[0]
 
-  output_format["ext"] = "tiff"
+  output_format["ext"] = entry.key.split(".")[-1]
   output_file = "/tmp/{0:s}".format(util.file_name(output_format))
   with open(output_file, "wb+") as f:
     entry.download(f)
@@ -31,30 +61,63 @@ def run(database: Database, key: str, params, input_format, output_format, offse
   more = True
   while more:
     [items, _, more] = it.next()
-    # Currently, evaluating using majority rules
     for [point, neighbors] in items:
       [x, y] = list(map(lambda p: int(p), point.split(b' ')))
       scores = [0, 0, 0]
       neighbors = sorted(neighbors, key=lambda n: n[0])
       d1 = neighbors[0][0]
       dk = neighbors[-1][0]
+      denom = dk - d1 if dk != d1 else 0.0001
       for i in range(len(neighbors)):
         [d, c] = neighbors[i]
-        w = 1 if i == 0 else (dk - d) / (dk - d1)
+        w = 1 if i == 0 else (dk - d) / denom
         scores[c] += w
       m = max(scores)
       top = [i for i, j in enumerate(scores) if j == m]
       if len(top) == 1:
-        if top[0] == Indices.border.value:
-          im.putpixel((x, y), (255, 0, 0))
-        elif top[0] == Indices.inside.value:
-          im.putpixel((x, y), (255, 255, 0))
         classifications[y][x] = top[0]
       else:
         classifications[y][x] = Indices.outside.value
 
-  print(classifications)
-  print("Saving to output_file", output_file)
+  for y in range(height):
+    for x in range(width):
+      if classifications[y][x] == Indices.border.value:
+        counts = [0, 0, 0]
+        for dy in range(-1, 2):
+          for dx in range(-1, 2):
+            iy = y + dy
+            ix = x + dx
+            if (0 <= iy and iy < height) and (0 <= ix and ix < width):
+              counts[classifications[iy][ix]] += 1
+        if counts[Indices.border.value] <= 1:
+          classifications[y][x] = Indices.outside.value
+
+  visited = {}
+  for y in range(height):
+    for x in range(width):
+      if x in visited and y in visited[x]:
+        continue
+
+      if classifications[y][x] == Indices.outside.value:
+        counts = [0, 0, 0]
+        total = 0
+        for dy in range(-1, 2):
+          for dx in range(-1, 2):
+            iy = y + dy
+            ix = x + dx
+            if (0 <= iy and iy < height) and (0 <= ix and ix < width):
+              counts[classifications[iy][ix]] += 1
+            total += 1
+        if counts[Indices.outside.value] == total:
+          flood(classifications, x, y, visited, width, height, Indices.outside.value)
+
+  for y in range(height):
+    for x in range(width):
+      if classifications[y][x] == Indices.border.value:
+        im.putpixel((x, y), (255, 0, 0))
+      elif classifications[y][x] == Indices.inside.value:
+        im.putpixel((x, y), (255, 255, 0))
+
   im.save(output_file)
 
   return [output_file]
