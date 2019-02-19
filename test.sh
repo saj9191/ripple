2adb704394fdffea03184056d2598bcbc2ae98f6
Shannon <sj677@cornell.edu>
2019-02-18 23:32:31 -0500

    standardize application test names

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ "$1" == "format" ]; then
+if [ "$1" == "applications" ]; then
+  pattern="applications_*_test.py"
+elif [ "$1" == "format" ]; then
   pattern="format_*_test.py"
+elif [ "$1" == "pipeline" ]; then
+  pattern="pipeline_*_test.py"
 else
 	pattern="*_test.py"
 fi
