22d8fec63c86c2336fbe97f552f361c7f92f0278
Shannon <sj677@cornell.edu>
2019-02-28 14:09:32 -0500

    simulation: Take arguments from json file

--- scheduler/simulation.py
+++ scheduler/simulation.py
@@ -12,26 +12,36 @@ sys.path.insert(0, parentdir)
 import setup
 
 
-def create_jobs(policy):
-  source_bucket = "tide-source-data"
-  destination_bucket = "maccoss-tide"
-  prefix = "DIA-Files/"
+def create_jobs(source_bucket, destination_bucket, policy, prefix, num_jobs, job_duration, relative):
   s3 = boto3.resource("s3")
   bucket = s3.Bucket(source_bucket)
   objs = list(bucket.objects.filter(Prefix=prefix))
-  objs = list(filter(lambda obj: obj.key.endswith("mzML"), objs))
-  job_duration = 180
+  objs = list(filter(lambda obj: not obj.key.endswith("/"), objs))
+  now = 0 if relative else time.time() + 10
 
   if policy == "deadline":
     jobs = []
     offset = 60
-    now = time.time() + 10
-    num_jobs = 10
     for i in range(num_jobs):
       obj = objs[i]
-      jobs.append(scheduler.Job(source_bucket, destination_bucket, obj.key, start_time=now + i * offset, deadline=now + job_duration + (2*(num_jobs-1)- i) * offset))
+      print(obj.key)
+      start_time = now + i * offset
+      deadline = now + job_duration + (2 * (num_jobs - 1) - i) * offset
+      jobs.append(scheduler.Job(source_bucket, destination_bucket, obj.key, start_time=start_time, deadline=deadline))
   elif policy == "priority":
-    raise Exception("Not implemented")
+    jobs = []
+    offset = 40
+    duration = 200
+    for i in range(num_jobs):
+      obj = objs[i]
+      start_time = now + i * duration
+      if i % 3 == 2:
+        priority = 10
+        start_time = now + (i - 1) * duration + offset
+      else:
+        priority = 1
+      jobs.append(scheduler.Job(source_bucket, destination_bucket, obj.key, start_time=start_time, priority=priority))
+
   jobs = scheduler.simulation_order(jobs, policy, job_duration, 1)
   jobs = list(map(lambda j: j[0], jobs))
   for job in jobs:
@@ -42,15 +52,18 @@ def create_jobs(policy):
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("--parameters", type=str, required=True, help="File containing parameters")
-  parser.add_argument("--policy", type=str, default="fifo", help="Scheduling policy to use (fifo, robin, deadline)")
-  parser.add_argument("--timeout", type=int, default=60, help="How long we should wait for a task to retrigger")
+  parser.add_argument("--run", action="store_true", default=False, help="Run simulation")
+  parser.add_argument("--relative", action="store_true", default=False, help="Store timestamps assuming first timestamp is at time 0")
   args = parser.parse_args()
-  params = json.loads(open(args.parameters).read())
+
+  p = json.loads(open(args.parameters).read())
+  params = json.loads(open(p["parameters"]).read())
   setup.process_functions(params)
-  jobs = create_jobs(args.policy)
-  s = scheduler.Scheduler(args.policy, args.timeout, params)
-  s.add_jobs(jobs)
-  s.listen()
+  jobs = create_jobs(p["source_bucket"], params["bucket"], p["policy"], p["prefix"], p["num_jobs"], p["duration"], args.relative)
+  if args.run:
+    s = scheduler.Scheduler(p["policy"], p["timeout"], params)
+    s.add_jobs(jobs)
+    s.listen()
 
 
 main()
