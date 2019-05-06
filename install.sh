61b913c652492efa5b21a97e46d852d1eceea11e
Shannon <sj677@cornell.edu>
2019-05-06 11:14:41 -0400

    install: Add other directories

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ python -m pip install --upgrade pip
 python -m pip install -U mypy
 python -m pip install boto3
 
-cmd="export PYTHONPATH=\$PYTHONPATH:$PWD"
+cmd="export PYTHONPATH=\$PYTHONPATH:$PWD:$PWD/applications:$PWD/lambda:$PWD/formats"
 echo $cmd >> ~/.bashrc
