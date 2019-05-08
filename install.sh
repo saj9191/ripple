f5bd029f7edbf9e71cfa52b5bd8b2d974dd3e631
Shannon Joyner <shannonjoyner@dhcp-rhodes-183.eduroam.cornell.edu>
2019-05-08 11:38:24 -0400

    Check that we have at least python 3.6

--- install.sh
+++ install.sh
@@ -5,10 +5,18 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
-version=`python -c 'import sys; print(sys.version_info[:][0])'`
-if [ "$version" -lt 3 ]
-  then echo "Please upgrade to Python 3"
+version=`python -c 'import sys; print(sys.version_info[:][0:2])'`
+if [ ${version:1:1} -lt 3 ]
+  then
+  echo "Please upgrade to Python 3.6"
   exit
+elif [ ${version:1:1} -eq 3 ]
+  then
+  if [ ${version:4:1} -lt 6 ]
+    then
+    echo "Please upgrade to Python 3.6"
+    exit
+  fi
 fi
 
 python -m pip --version
