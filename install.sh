03e09b0e964eadb3af4ffdeeff8de09f8beb58d4
Shannon Joyner <shannonjoyner@dhcp-rhodes-183.eduroam.cornell.edu>
2019-05-08 10:58:31 -0400

    Have user install python3

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
+version=`python -c 'import sys; print(sys.version_info[:][0])'`
+if [ "$version" -lt 3 ]
+  then echo "Please upgrade to Python 3"
+  exit
+fi
+
 python -m pip install --upgrade pip
 python -m pip install -U mypy
 python -m pip install boto3
