775a82cbd0c2e97fb526f7c30faf0bfa1ab16585
Shannon Joyner <shannonjoyner@garyjor-surbook.northamerica.corp.microsoft.com>
2019-09-26 19:41:51 -0700

    Split tmt6 and tmt10 apart

--- applications/parammedic.py
+++ applications/parammedic.py
@@ -6,7 +6,8 @@ from database.database import Database
 ITRAQ = re.compile("INFO: iTRAQ: ([0-9]+)-plex reporter ions detected")
 SILAC = re.compile("INFO: SILAC: ([0-9]+)Da separation detected.")
 PHOSPHORYLATION = re.compile("INFO: Phosphorylation: detected")
-TMT = re.compile("INFO: TMT: ([0-9]+)-plex reporter ions detected")
+TMT6 = re.compile("INFO: TMT: 6-plex reporter ions detected")
+TMT10 = re.compile("INFO: TMT: 10-plex reporter ions detected")
 
 
 def run(database: Database, file: str, params, input_format, output_format):
@@ -20,14 +21,20 @@ def run(database: Database, file: str, params, input_format, output_format):
   phos = PHOSPHORYLATION.search(output)
   itraq = ITRAQ.search(output)
   silac = SILAC.search(output)
-  tmt = TMT.search(output)
+  tmt6 = TMT6.search(output)
+  tmt10 = TMT10.search(output)
 
   map_bucket = None
-  if tmt:
+  if tmt6:
     if phos:
-      map_bucket = "maccoss-tmt-phosphorylation-fasta"
+      map_bucket = "maccoss-tmt6-phosphorylation-fasta"
     else:
-      map_bucket = "maccoss-tmt-fasta"
+      map_bucket = "maccoss-tmt6-fasta"
+  elif tmt10:
+    if phos:
+      map_bucket = "maccoss-tmt10-phosphorylation-fasta"
+    else:
+      map_bucket = "maccoss-tmt10-fasta"
   elif itraq:
     if phos:
       map_bucket = "maccoss-itraq-phosphorylation-fasta"
